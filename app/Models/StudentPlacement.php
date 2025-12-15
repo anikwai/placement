73781b48c4f76cfac075f133d1b3c1a2117a525c
anikwai <anikwai.w@gmail.com>
2025-12-16 00:33:32 +1100

    Add StudentPlacement model for managing student placement data
    This new model includes fillable attributes for student information and a method to convert the model data into a searchable array, enhancing the application's search capabilities.

---
+++ app/Models/StudentPlacement.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+
+class StudentPlacement extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentPlacementFactory> */
+    use HasFactory, Searchable;
+
+    /**
+     * @var list<string>
+     */
+    protected $fillable = [
+        'national_student_id',
+        'feeder_school_name',
+        'student_name',
+        'gender',
+        'year_7_placement_school_name',
+    ];
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array<string, mixed>
+     */
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => (string) $this->id,
+            'national_student_id' => $this->national_student_id,
+            'student_name' => $this->student_name,
+            'feeder_school_name' => $this->feeder_school_name,
+            'gender' => $this->gender,
+            'year_7_placement_school_name' => $this->year_7_placement_school_name,
+            'created_at' => $this->created_at->timestamp,
+        ];
+    }
+}
