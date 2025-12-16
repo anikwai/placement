7ae29a848f335db616cc77e81343bde3b1ab75db
anikwai <anikwai.w@gmail.com>
2025-12-16 15:51:28 +1100

    feat: Implement global placement search, enhance dashboard with new components and tests, and refactor placement views.

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,9 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'ui' => [
+                'logoText' => 'Placement',
+            ],
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
